f2fe940e318a4c953262434d351feef588cfe455
ep-whwong <wh.wong@entrypass.net>
2024-03-29 21:44:18 +0800

    Changes required in order to be able to be compiled in ESP IDF v.5.2.1

--- demos/esp32_spi_flash/main/main.c
+++ demos/esp32_spi_flash/main/main.c
@@ -138,15 +138,15 @@ void app_main()
     printf("FlashDB ESP32 SPI Flash Demo\n");
 
     /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi, ",
-           chip_info.cores);
+    // esp_chip_info_t chip_info;
+    // esp_chip_info(&chip_info);
+    // printf("This is ESP32 chip with %d CPU cores, WiFi, ",
+    //        chip_info.cores);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    // printf("silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    // printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     flashdb_demo();
 
